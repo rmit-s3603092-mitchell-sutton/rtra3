dfbfc7b81b7336092fa6ff4fb9b5d16f277aefd8
rmit-s3603092-mitchell-sutton <s3603092@student.rmit.edu.au>
2018-09-17 18:56:25 +1000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  rtra3
+//
+//  Created by mitcsutt on 17/9/18.
+//  Copyright © 2018 Mitchell Sutton. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
